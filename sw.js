85f32af81b37878d5734a21353cd094961a2a853
Gabriel Khamassi <gabriel.khamassi@free.fr>
2026-05-29 16:12:00 +0200

    PWA : sw.js precache 54 modules complets + Promise.allSettled resilient

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'plaqpro-v20260529';
+const CACHE_NAME = 'plaqpro-v20260529b';
 
 const FICHIERS_CACHE = [
   '/plaqpro-web/login.html',
@@ -10,17 +10,57 @@ const FICHIERS_CACHE = [
   '/plaqpro-web/js/db.js',
   '/plaqpro-web/js/calculs.js',
   '/plaqpro-web/js/app.js',
+  '/plaqpro-web/js/alertes.js',
   '/plaqpro-web/js/calculateur.js',
   '/plaqpro-web/js/liste_achat.js',
   '/plaqpro-web/js/page_cloisons.js',
   '/plaqpro-web/js/page_peinture.js',
-  '/plaqpro-web/js/produits_complet.js',
   '/plaqpro-web/js/tarifs.js',
-  '/plaqpro-web/js/pdf_export.js',
   '/plaqpro-web/js/memo_oublis.js',
-  '/plaqpro-web/js/alertes.js',
-  '/plaqpro-web/js/signature.js',
+  '/plaqpro-web/js/pdf_export.js',
+  '/plaqpro-web/js/excel_export.js',
+  '/plaqpro-web/js/produits_complet.js',
+  '/plaqpro-web/js/produits_web.js',
+  '/plaqpro-web/js/assistant_ia.js',
+  '/plaqpro-web/js/facturation.js',
+  '/plaqpro-web/js/document_print.js',
+  '/plaqpro-web/js/quiz_plaquiste.js',
+  '/plaqpro-web/js/calculatrice.js',
+  '/plaqpro-web/js/meteo.js',
+  '/plaqpro-web/js/graphiques.js',
+  '/plaqpro-web/js/calendrier.js',
+  '/plaqpro-web/js/prospection.js',
+  '/plaqpro-web/js/pack_maconnerie.js',
+  '/plaqpro-web/js/pack_electricite.js',
+  '/plaqpro-web/js/pack_plomberie.js',
+  '/plaqpro-web/js/pack_exterieur.js',
+  '/plaqpro-web/js/analyse_photo.js',
+  '/plaqpro-web/js/pause_cafe.js',
+  '/plaqpro-web/js/dpgf.js',
+  '/plaqpro-web/js/aide.js',
+  '/plaqpro-web/js/mode_chantier.js',
+  '/plaqpro-web/js/projets_types.js',
+  '/plaqpro-web/js/devis_multi.js',
   '/plaqpro-web/js/email_devis.js',
+  '/plaqpro-web/js/signature.js',
+  '/plaqpro-web/js/acoustique.js',
+  '/plaqpro-web/js/thermique.js',
+  '/plaqpro-web/js/section_cable.js',
+  '/plaqpro-web/js/temps_chantier.js',
+  '/plaqpro-web/js/rentabilite.js',
+  '/plaqpro-web/js/resistance_feu.js',
+  '/plaqpro-web/js/linteau.js',
+  '/plaqpro-web/js/regles.js',
+  '/plaqpro-web/js/sous_traitants.js',
+  '/plaqpro-web/js/mon_compte.js',
+  '/plaqpro-web/js/inscription.js',
+  '/plaqpro-web/js/legal.js',
+  '/plaqpro-web/js/catalogue_fournisseurs.js',
+  '/plaqpro-web/js/quiz_metiers.js',
+  '/plaqpro-web/js/charges.js',
+  '/plaqpro-web/js/devis_intelligent.js',
+  '/plaqpro-web/js/import_catalogue.js',
+  '/plaqpro-web/js/carte_premium.js',
   '/plaqpro-web/assets/logo_plaqpro.png',
   '/plaqpro-web/assets/icon-192.png',
   '/plaqpro-web/assets/icon-512.png',
@@ -29,7 +69,9 @@ const FICHIERS_CACHE = [
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FICHIERS_CACHE))
+      .then(cache => Promise.allSettled(
+        FICHIERS_CACHE.map(f => cache.add(f).catch(() => null))
+      ))
       .then(() => self.skipWaiting())
   );
 });
